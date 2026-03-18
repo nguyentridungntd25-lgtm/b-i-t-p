7d4ada9bfed1a3ce997f1d427ed95f6c7abe12d2
nguyentridungntd25-lgtm <nguyentridungntd25@gmail.com>
2026-03-18 11:05:51 +0700

    Add files via upload

---
+++ Ex7_4.java
@@ -0,0 +1,15 @@
+package ra.edu;
+
+public class Ex7_4 {
+    public class Book {
+        public String title;
+        public String author;
+        public double price;
+
+        public void printInfo() {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("Price: " + price);
+        }
+    }
+}
